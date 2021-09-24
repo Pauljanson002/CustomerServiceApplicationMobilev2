553197f4936be92d032423808a4f78b7c8a412cc
Paul Janson <pauljanson002@gmail.com>
2021-09-24 17:40:36 +0530

    Profile Added

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import MainApp from './src/Main';
+import {NativeBaseProvider} from "native-base";
 
 export default function App() {
-    return <MainApp />;
+    return(<NativeBaseProvider>
+        <MainApp />
+    </NativeBaseProvider>)
 }
--- src/screens/my_profile_screen.js
+++ src/screens/my_profile_screen.js
@@ -1,9 +1,12 @@
 import React from "react";
-import {Text,View,Button} from "react-native";
+import {View,Button} from "react-native";
 import * as SecureStore from "expo-secure-store";
 import {useQuery} from "@apollo/client";
 import {GET_ME} from "../gql/query";
-import {Box} from "@jrobins/bulma-native"
+import {Center, Container, Heading, Text, Avatar, VStack, HStack, Spacer} from "native-base";
+import Box from "native-base/src/components/primitives/Box/index";
+import Stack from "native-base/src/components/primitives/Stack/Stack";
+
 
 const MyProfileScreen = (props)=>{
     const signOut = () => {
@@ -15,11 +18,56 @@ const MyProfileScreen = (props)=>{
     if(meQuery.loading) return <Text>Loading</Text>
     if(meQuery.error) return <Text>Error</Text>
     return(
-        <View>
-                <Text>{meQuery.data.me.fullname}</Text>
-                <Text>{meQuery.data.me.username}</Text>
-                <Button title="Sign Out" onPress={signOut} />
-        </View>
+        <Box>
+            <Center>
+                <Stack mt={5} space={3}>
+                    <Center>
+                        <VStack space={3}>
+                            <Heading textAlign={"center"}>{meQuery.data.me.fullname}</Heading>
+                            <Heading>@{meQuery.data.me.username}</Heading>
+                            <Center>
+                                <Avatar>{meQuery.data.me.fullname[0]}</Avatar>
+                            </Center>
+                        </VStack>
+                    </Center>
+                    <HStack alignItems={"center"} space={5} mt={5}>
+                        <Text fontWeight={"bold"}>Address</Text>
+                        <Spacer/>
+                        <Text>{meQuery.data.me.address}</Text>
+                    </HStack>
+                    <HStack alignItems={"center"} space={5} mt={5}>
+                        <Text fontWeight={"bold"}>Email</Text>
+                        <Spacer/>
+                        <Text>{meQuery.data.me.email}</Text>
+                    </HStack>
+                    <HStack alignItems={"center"} space={5} mt={5}>
+                        <Text fontWeight={"bold"}>City</Text>
+                        <Spacer/>
+                        <Text>{meQuery.data.me.city}</Text>
+                    </HStack>
+                    <HStack alignItems={"center"} space={5} mt={5}>
+                        <Text fontWeight={"bold"}>Town</Text>
+                        <Spacer/>
+                        <Text>{meQuery.data.me.town}</Text>
+                    </HStack>
+                    <HStack alignItems={"center"} space={5} mt={5}>
+                        <Text fontWeight={"bold"}>Province</Text>
+                        <Spacer/>
+                        <Text>{meQuery.data.me.province}</Text>
+                    </HStack>
+                    <HStack alignItems={"center"} space={5} mt={5}>
+                        <Text fontWeight={"bold"}>Bio</Text>
+                        <Spacer/>
+                        <Text>{meQuery.data.me.bio}</Text>
+                    </HStack>
+                </Stack>
+            </Center>
+            <Container>
+
+            </Container>
+        </Box>
+
+
     )
 }
 
