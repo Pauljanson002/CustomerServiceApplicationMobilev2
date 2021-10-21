13ac1d292dcbc7079c4331f96f842f4e13937d77
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2021-10-21 21:36:44 +0530

    sorting and filtering ft

--- src/screens/providersByProfession.js
+++ src/screens/providersByProfession.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon } from 'react-native-elements';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, NetworkStatus } from '@apollo/client';
 import {
   StyleSheet,
   Text,
@@ -8,44 +8,140 @@ import {
   FlatList,
   TouchableOpacity,
 } from 'react-native';
+import { Select, VStack, CheckIcon, HStack, Checkbox } from 'native-base';
 import { Rating, AirbnbRating } from 'react-native-ratings';
 //import { AppLoading } from 'expo';
 //import { useFonts, Inter_200ExtraLight } from '@expo-google-fonts/inter';
-const GET_SERVICE_PROVIDER_BY_PROFESSION = gql`
-  query SearchServiceProviderByProfession(
-    $searchServiceProviderbyProfessionProfession: String!
+const GET_PROVIDERS_BY_PROFESSION_IN_PROVINCE = gql`
+  query Query(
+    $searchServiceProviderbyProfessioninProvinceProfession: String!
+    $searchServiceProviderbyProfessioninProvinceProvince: String
+    $searchServiceProviderbyProfessioninProvinceCity: String
+    $searchServiceProviderbyProfessioninProvinceRating: String
   ) {
-    searchServiceProviderbyProfession(
-      profession: $searchServiceProviderbyProfessionProfession
+    searchServiceProviderbyProfessioninProvince(
+      profession: $searchServiceProviderbyProfessioninProvinceProfession
+      province: $searchServiceProviderbyProfessioninProvinceProvince
+      city: $searchServiceProviderbyProfessioninProvinceCity
+      rating: $searchServiceProviderbyProfessioninProvinceRating
     ) {
       id
       username
       fullname
       profession
+      province
+      postalCode
       city
       bio
-      service_providing_status
       roles
-      postalCode
+      service_providing_status
       provider_rating
       provider_review_count
     }
   }
 `;
+
+const GET_ME = gql`
+  query Query {
+    me {
+      id
+      username
+      fullname
+      email
+      nic
+      profession
+      contactNum
+      address
+      province
+      city
+      town
+      bio
+      service_providing_status
+      roles
+      postalCode
+    }
+  }
+`;
+
 const ProvidersByProfessionScreen = ({ navigation, route }) => {
   //const userQuery = useQuery(GET_USERS)
 
   const profession = navigation.getParam('profession');
-  const providersQuery = useQuery(GET_SERVICE_PROVIDER_BY_PROFESSION, {
-    variables: {
-      searchServiceProviderbyProfessionProfession: profession,
-    },
-  });
+  const [checked, setChecked] = useState(false);
+  const [citychecked, setCityChecked] = useState(false);
+  const [values, setValues] = useState({ profession: '' });
+  const [city, setCity] = useState('');
+  const [province, setProvince] = useState('');
+  const [rating, setRating] = useState('');
+  const { loading, error, data, refetch, networkStatus } = useQuery(
+    GET_PROVIDERS_BY_PROFESSION_IN_PROVINCE,
+    {
+      variables: {
+        searchServiceProviderbyProfessioninProvinceProfession: `${profession}`,
+        searchServiceProviderbyProfessioninProvinceProvince: province,
+        searchServiceProviderbyProfessioninProvinceCity: city,
+        searchServiceProviderbyProfessioninProvinceRating: rating,
+      },
+      notifyOnNetworkStatusChange: true,
+    }
+  );
+  const meQuery = useQuery(GET_ME);
+  if (networkStatus === NetworkStatus.refetch) return 'Refetching...';
+  if (loading || meQuery.loading) return <Text>Loading</Text>;
 
-  if (providersQuery.loading) return <Text>Loading</Text>;
+  const providersResult = data.searchServiceProviderbyProfessioninProvince;
+  //console.log(meQuery);
 
-  const providersResult = providersQuery.data.searchServiceProviderbyProfession;
-  console.log(profession);
+  const handleChange = (itemValue) => {
+    console.log(itemValue);
+    setRating(itemValue);
+    setValues({
+      ...values,
+      searchServiceProviderbyProfessioninProvinceRating: itemValue,
+    });
+  };
+
+  const handleCheck = (event) => {
+    setChecked(!checked);
+    console.log(checked, 'province', meQuery.data.me.province);
+    if (!checked) {
+      setProvince(meQuery.data.me.province);
+      setValues({
+        ...values,
+        searchServiceProviderbyProfessioninProvinceProvince:
+          meQuery.data.me.province,
+      });
+    } else {
+      setProvince('');
+      setValues({
+        ...values,
+        searchServiceProviderbyProfessioninProvinceProvince: '',
+      });
+    }
+
+    refetch();
+  };
+
+  const handleCityCheck = (event) => {
+    setCityChecked(!citychecked);
+    console.log(citychecked, 'city');
+    if (!citychecked) {
+      setCity(meQuery.data.me.city);
+      console.log(city);
+      setValues({
+        ...values,
+        searchServiceProviderbyProfessioninProvinceCity: meQuery.data.me.city,
+      });
+    } else {
+      console.log(citychecked, 'ciy');
+      setCity('');
+      setValues({
+        ...values,
+        searchServiceProviderbyProfessioninProvinceCity: '',
+      });
+    }
+    refetch();
+  };
 
   function Item({ item }) {
     return (
@@ -58,7 +154,7 @@ const ProvidersByProfessionScreen = ({ navigation, route }) => {
             @{item.username}
           </Text>
 
-          <Text style={{ margin: 6, fontSize: 16, color:'#525252' }}>
+          <Text style={{ margin: 6, fontSize: 16, color: '#525252' }}>
             <Icon
               name="google-maps"
               type="material-community"
@@ -67,24 +163,27 @@ const ProvidersByProfessionScreen = ({ navigation, route }) => {
             {item.city} - {item.postalCode}
           </Text>
           <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-          }}
-        >
-          <Rating
-            type="star"
-            ratingCount={5}
-            imageSize={20}
-            isDisabled={true}
-            readonly={true}
-            startingValue={item.provider_rating}
-           
-          /> 
-          <Text style={{ margin: 6, fontSize: 16 , color:'#525252' }}>{item.provider_review_count} Reviews</Text>
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+            }}
+          >
+            <Rating
+              type="star"
+              ratingCount={5}
+              imageSize={20}
+              isDisabled={true}
+              readonly={true}
+              startingValue={item.provider_rating}
+            />
+            <Text style={{ margin: 6, fontSize: 16, color: '#525252' }}>
+              {item.provider_review_count} Reviews
+            </Text>
           </View>
 
-          <Text style={{ margin: 6, fontSize: 16, color:'#525252'  }}>{item.bio}</Text>
+          <Text style={{ margin: 6, fontSize: 16, color: '#525252' }}>
+            {item.bio}
+          </Text>
         </View>
         <View
           style={{
@@ -98,11 +197,10 @@ const ProvidersByProfessionScreen = ({ navigation, route }) => {
               width: 157,
               justifyContent: 'center',
               alignItems: 'center',
-              borderColor:'#525252',
+              borderColor: '#525252',
               borderRadius: 8,
-              borderWidth:0.5,
+              borderWidth: 0.5,
               margin: 4,
-              
             }}
             onPress={() => navigation.navigate('HireNow', { id: item.id })}
           >
@@ -115,8 +213,8 @@ const ProvidersByProfessionScreen = ({ navigation, route }) => {
               width: 157,
               justifyContent: 'center',
               alignItems: 'center',
-              borderColor:'#525252',
-              borderWidth:0.5,
+              borderColor: '#525252',
+              borderWidth: 0.5,
               borderRadius: 8,
               margin: 4,
             }}
@@ -132,16 +230,81 @@ const ProvidersByProfessionScreen = ({ navigation, route }) => {
     );
   }
   return (
-    <View>
-      <Text>
-        Showing {providersResult.length} Results for {profession}
-      </Text>
+    <View style={{ backgroundColor: 'white' }}>
+      <HStack>
+        <Icon
+          name="hammer-screwdriver"
+          type="material-community"
+          color="#517fa4"
+          style={{
+            color: '#525252',
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginTop: 10,
+            marginRight:8
+          }}
+        />
+        <Text
+          style={{
+            color: '#525252',
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginTop: 10,
+          }}
+        >
+          Showing {profession}s
+        </Text>
+      </HStack>
+      <Select
+        selectedValue={rating}
+        minWidth="190"
+        minHeight="50"
+        accessibilityLabel="Sort"
+        placeholder="Sort by Rating"
+        padding={4}
+        _selectedItem={{
+          bg: '#bae6fd',
+          endIcon: <CheckIcon size="5" />,
+        }}
+        mt={4}
+        onValueChange={(itemValue) => handleChange(itemValue)}
+      >
+        <Select.Item label="Highest to Lowest" value="0" />
+        <Select.Item label="Lowest to Highest" value="1" />
+      </Select>
+      <HStack space={6}>
+        <Checkbox
+          accessibilityLabel="province checkbox"
+          onChange={handleCheck}
+          isChecked={checked}
+          name={'searchServiceProviderbyProfessioninProvinceProvince'}
+          marginLeft={4}
+        />
+        <Text style={{ color: '#525252', fontSize: 16 }}>
+          Show providers only in my Province
+        </Text>
+      </HStack>
+      <HStack space={6}>
+        <Checkbox
+          accessibilityLabel="city checkbox"
+          onChange={handleCityCheck}
+          isChecked={citychecked}
+          name={'searchServiceProviderbyProfessioninProvinceCity'}
+          marginLeft={4}
+          marginTop={1}
+          marginBottom={1}
+        />
+        <Text style={{ color: '#525252', fontSize: 16, marginTop: 8 }}>
+          Show providers only in my City
+        </Text>
+      </HStack>
 
       <FlatList
         data={providersResult}
         renderItem={({ item }) => <Item item={item} />}
         keyExtractor={(item) => item.id}
       />
+      
     </View>
   );
 };
